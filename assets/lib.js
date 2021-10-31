4c66c04c9b859bc4a2e6537e5c978b06a4f17be6
EntityPlantt <92634082+EntityPlantt@users.noreply.github.com>
2021-10-31 15:06:17 +0100

    Cookies -> Local storage
    Cookies can't hold much value... so let's use local storage.

--- assets/lib.js
+++ assets/lib.js
@@ -23,27 +23,13 @@ function saveFile(name, extension, content) {
   downloadLink.click();
 }
 function setCookie(cname, cvalue, exdays) {
-  const d = new Date();
-  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-  let expires = "expires=" + d.toUTCString();
-  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  window.localStorage.setItem(cname, cvalue);
 }
 function getCookie(cname) {
-  let name = cname + "=";
-  let ca = document.cookie.split(';');
-  for (let i = 0; i < ca.length; i++) {
-    let c = ca[i];
-    while (c.charAt(0) == ' ') {
-      c = c.substring(1);
-    }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
-  }
-  return null;
+  return window.localStorage.getItem(cname);
 }
 function delCookie(cname) {
-  setCookie(cname, null, -1);
+  window.localStorage.removeItem(cname);
 }
 function isValidRegex(str) {
   return /^\w+$/.test(str);
